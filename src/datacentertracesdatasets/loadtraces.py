937fd26e13c60cc0f8d3beb2c9259437d8d5cbce
alejandrofdez-us <afdez@us.es>
2023-12-14 14:02:15 +0100

    Added synthetic traces generation of machine usage for 1 day and sampled at 300 sgs
    Co-authored-by: Damián Fernández Cerero <damiancerero@us.es>

--- src/datacentertracesdatasets/loadtraces.py
+++ src/datacentertracesdatasets/loadtraces.py
@@ -1,51 +1,67 @@
 import pandas as pd
 from io import StringIO
+import random
+from importlib import import_module
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    import importlib_resources as pkg_resources
 
-def __get_alibaba_2018_trace(trace="machine_usage", stride_seconds=10):
-    try:
-        import importlib.resources as pkg_resources
-    except ImportError:
-        import importlib_resources as pkg_resources
-    from . import alibaba2018
+def __get_random_csv_file_from_folder(package):
+    synthetic_csv_files = [file for file in pkg_resources.contents(package) if file.endswith('.csv')]
+    random_synthetic_csv_file = random.choice(synthetic_csv_files)
+    return pkg_resources.read_text(package, random_synthetic_csv_file)
+
+def __get_alibaba_2018_trace(trace="machine_usage", stride_seconds=10, generation_strategy='real'):
     assert trace in ["machine_usage"], 'only "machine_usage" trace is supported right now for alibaba'
     assert stride_seconds in [10, 30, 300], 'only 10, 30 and 300 seconds are currently suported as stride_seconds in alibaba 2018 machine usage'
-    contents = pkg_resources.read_text(alibaba2018, f'{trace}_grouped_{stride_seconds}_seconds.csv')
+    assert generation_strategy in ["real", "synthetic"], 'only real and synthetic generation strategies are currently supported in alibaba 2018 machine usage'
+    if generation_strategy == "real":
+        module = import_module(f".alibaba2018", package=__package__)
+        contents = pkg_resources.read_text(module, f'{trace}_grouped_{stride_seconds}_seconds.csv')
+    else:
+        assert stride_seconds in [300], 'only 300 seconds are currently suported as stride_seconds in synthetically-generated alibaba 2018 machine usage'
+        submodule = import_module(f".alibaba2018.synthetic_{1}_day_{stride_seconds}_seconds", package=__package__)
+        contents = __get_random_csv_file_from_folder(submodule)
     return pd.read_csv(StringIO(contents), index_col=False)
 
 
-def __get_azure_v2_trace(trace="machine_usage", stride_seconds=300):
-    try:
-        import importlib.resources as pkg_resources
-    except ImportError:
-        import importlib_resources as pkg_resources
-    from . import azure_v2
+def __get_azure_v2_trace(trace="machine_usage", stride_seconds=300, generation_strategy='real'):
     assert trace in ["machine_usage"], 'only "machine_usage" trace is supported right now for azure_v2'
     assert stride_seconds in [300], 'only 300 seconds is currently supported as stride_seconds in azure_v2'
-    contents = pkg_resources.read_text(azure_v2, f'{trace}_grouped_{stride_seconds}_seconds.csv')
+    assert generation_strategy in ["real", "synthetic"], 'only real and synthetic generation strategies are currently supported in azure_v2 machine usage'
+    if generation_strategy == "real":
+        module = import_module(f".azure_v2", package=__package__)
+        contents = pkg_resources.read_text(module, f'{trace}_grouped_{stride_seconds}_seconds.csv')
+    else:
+        submodule = import_module(f".azure_v2.synthetic_{1}_day_{stride_seconds}_seconds", package=__package__)
+        contents = __get_random_csv_file_from_folder(submodule)
     return pd.read_csv(StringIO(contents), index_col=False)
 
-def __get_google_2019_trace(trace="machine_usage", stride_seconds=300):
-    try:
-        import importlib.resources as pkg_resources
-    except ImportError:
-        import importlib_resources as pkg_resources
-    from . import google2019
+
+def __get_google_2019_trace(trace="machine_usage", stride_seconds=300, generation_strategy='real'):
     assert trace in ["machine_usage"], 'only "machine_usage" trace is supported right now for azure_v2'
     assert stride_seconds in [300], 'only 300 seconds is currently suported as stride_seconds in google2019'
-    contents = pkg_resources.read_text(google2019, f'{trace}_grouped_{stride_seconds}_seconds_percent.csv')
+    assert generation_strategy in ["real", "synthetic"], 'only real and synthetic generation strategies are currently supported in google2019 machine usage'
+    if generation_strategy == "real":
+        module = import_module(f".google2019", package=__package__)
+        contents = pkg_resources.read_text(module, f'{trace}_grouped_{stride_seconds}_seconds_percent.csv')
+    else:
+        submodule = import_module(f".google2019.synthetic_{1}_day_{stride_seconds}_seconds", package=__package__)
+        contents = __get_random_csv_file_from_folder(submodule)
     return pd.read_csv(StringIO(contents), index_col=False)
 
-def get_trace(trace_name='alibaba2018', trace_type='machine_usage', stride_seconds=300, format='dataframe'):
+def get_trace(trace_name='alibaba2018', trace_type='machine_usage', stride_seconds=300, format='dataframe', generation_strategy='real'):
     assert trace_name in ['alibaba2018', 'azure_v2', 'google2019'], 'only alibaba2018, azure_v2 and google2019 traces are supported right now'
     assert trace_type in ['machine_usage'], 'only machine_usage traces are supported right now'
     assert stride_seconds in [10, 30, 300], 'only 10, 30 and 300 seconds are valid right now'
     assert format in ['dataframe', 'ndarray'], 'only "dataframe" and "ndarray" are supported right now'
     if trace_name == 'alibaba2018':
-        res = __get_alibaba_2018_trace(trace=trace_type, stride_seconds=stride_seconds)
+        res = __get_alibaba_2018_trace(trace=trace_type, stride_seconds=stride_seconds, generation_strategy=generation_strategy)
     elif trace_name == 'azure_v2':
-        res = __get_azure_v2_trace(trace=trace_type, stride_seconds=stride_seconds)
+        res = __get_azure_v2_trace(trace=trace_type, stride_seconds=stride_seconds, generation_strategy=generation_strategy)
     elif trace_name == 'google2019':
-        res = __get_google_2019_trace(trace=trace_type, stride_seconds=stride_seconds)
+        res = __get_google_2019_trace(trace=trace_type, stride_seconds=stride_seconds, generation_strategy=generation_strategy)
     if format == 'ndarray':
         res = res.to_numpy()
     return res
